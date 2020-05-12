b7cc92cfec79e143b7ebd6407ec51ca831036ff7
Michael-Anderson99 <xmichael299@gmail.com>
2020-05-12 02:47:58 +0100

    Updated views.py
    new function

--- Social/flicks/views.py
+++ Social/flicks/views.py
@@ -5,3 +5,5 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at my sites homepage")
 
+def hello(request):
+    return HttpResponse("helooooo")
